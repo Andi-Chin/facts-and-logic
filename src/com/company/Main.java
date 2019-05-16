fa635d3643fd6f043ca89ff07a8df262999daf8b
Andi-Chin <hellocyhandy@gmail.com>
2019-05-15 23:22:03 -0400

    nothing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        Gate gate1 = new Gate("nand", new GBWrapper(true), new GBWrapper(true));
+        Gate gate3 = new Gate("and", new GBWrapper(false), new GBWrapper(false));
+        Gate gate2 = new Gate("or", new GBWrapper(true), new GBWrapper(false));
+        Gate gate1 = new Gate("nand", new GBWrapper(gate2), new GBWrapper(gate3));
 
 
 
